af042b12204ee8762bf202df4ab94fb53d3361d6
Admin <Admin@DESKTOP-T8Q79FD>
2021-04-13 15:42:48 +0700

    Add project files.

---
+++ ListExercise/ListExercise/Demo.xaml.cs
@@ -0,0 +1,72 @@
+﻿using ListExercise.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListExercise
+{
+    
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Demo : ContentPage
+    {
+         static ObservableCollection<Search> searchs = new ObservableCollection<Search>
+            {
+                new Search{Id = 1,Location = "So 1 Thanh Xuan,Ha Noi",CheckIn = DateTime.Today,CheckOut = DateTime.Now},
+                new Search{Id = 2,Location = "52 Lac Long Quan,Ha Noi",CheckIn = DateTime.Today,CheckOut = DateTime.Now},
+                new Search{Id = 3, Location = "So 53 Trieu Khuc,Ha Noi", CheckIn = DateTime.Today, CheckOut = DateTime.Now}
+
+            };
+
+       
+        IEnumerable<Search> GetSearches (string searchText = null)
+        {
+            
+            if (String.IsNullOrWhiteSpace(searchText))
+                return searchs;
+            return searchs.Where(c => c.Location.StartsWith(searchText)).ToList();
+        }
+        
+        public Demo()
+        {
+            InitializeComponent();
+            listView.ItemsSource = GetSearches();
+            
+            
+          
+        }
+        
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            listView.ItemsSource = GetSearches(e.NewTextValue);
+        }
+
+      
+
+        private void Delete_Clicked(object sender, EventArgs e)
+        {
+            var search = (sender as MenuItem).CommandParameter as Search;
+            searchs.Remove(search);
+            listView.ItemsSource = GetSearches();
+            
+        }
+        private void listView_Refreshing(object sender, EventArgs e)
+        {
+            listView.ItemsSource = GetSearches();
+            listView.IsRefreshing = false;
+            listView.EndRefresh();
+
+        }
+
+        private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var search = e.SelectedItem as Search;
+            DisplayAlert("Selected", search.Location, "OK");
+        }
+    }
+}
---
+++ ListExercise/ListExercise/Models/Search.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListExercise.Models
+{
+    class Search
+    {
+        public int Id { get; set; }
+        public string Location { get; set; }
+        public DateTime CheckIn { get; set; }
+
+        public DateTime CheckOut { get; set; }
+
+
+
+    }
+}
